faa9aa8c312778b37d66cf100d764a5b5359b22b
Valentun Solodun <valentyn.solodun@smartexe.com>
2021-09-16 10:32:39 +0300

    Redesigned structure of app

---
+++ devtools.js
@@ -0,0 +1,9 @@
+const PANEL_CONFIG = {
+  title: 'XHRinfo',
+  iconPath: '',
+  pagePath: 'panel/index.html'
+};
+
+(function createNewPanel() {
+  window.chrome.devtools.panels.create(PANEL_CONFIG.title, PANEL_CONFIG.iconPath, PANEL_CONFIG.pagePath)
+}());
--- public/devtools.js
+++
@@ -1,9 +0,0 @@
-let backgroundPageConnection = window.chrome.runtime.connect({
-  name: "devtools-page"
-});
-
-window.chrome.devtools.panels.create(
-  'New panel',
-  '',
-  'panel.html'
-)
